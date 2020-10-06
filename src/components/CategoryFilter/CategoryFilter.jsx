8b409fd338d1b1a0d88f0ecde0e04cab70297e7a
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-10-06 15:59:27 -0300

    Filter

--- src/components/CategoryFilter/CategoryFilter.jsx
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -1,58 +1,28 @@
 import React from 'react';
-
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
 import {CategoryContainer} from  './styled'
 
 const CategoryFilter = (props) => {
     return (
         <CategoryContainer>
             <Tabs
-                value={"test"}
+                value={"selecionado"}
                 onChange={()=> null}
                 indicatorColor="primary"
-                textColor="primary"
+                textColor="inherit"
                 variant="scrollable"
                 scrollButtons="auto"
                 aria-label="scrollable auto tabs example"
             >
-            <Tab label="Item One"  />
-            <Tab label="Item Two"  />
-            <Tab label="Item Three"  />
-            <Tab label="Item Four"  />
-            <Tab label="Item Five"  />
-            <Tab label="Item Six"  />
-            <Tab label="Item Seven"  />
+              <Tab label="Pizza"  />
+              <Tab label="Hamburger" />
+              <Tab label="Sorvete"/>
+              <Tab label="Refeição"/>
+              <Tab label="Oriental"/>
+              <Tab label="Salgados"/>
+              <Tab label="Doces"/>
         </Tabs>
-      {/* </AppBar>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel> */}
-            
         </CategoryContainer>
     );
 }
