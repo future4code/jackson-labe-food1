ca7a77b6b50c3a0f5a0b11d13f2dd75c8972ec5c
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-10-07 14:26:50 -0300

    ajuste no App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import HomeScreen from './screens/Home/HomeScreen';
 import Router from './Routers/Routers';
 import Login from './screens/Login/Login';
 import SignUp from './screens/SignUp/SignUp';
-import CardDish from './components/CardDish/CardDish.jsx';
 
 function App() {
   return (
     < >
-    {/* <Router/> */}
-    <CardDish/>
+    <Router/>
     </>
   );
 }
