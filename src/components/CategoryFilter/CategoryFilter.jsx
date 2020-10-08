63011acdd82b06e3d7fcbbb5cc95bdc024f541ec
Flavia <afrodrigues38@gmail.com>
2020-10-08 11:47:04 -0300

    Rotas e cores dos botões do footer

--- src/components/CategoryFilter/CategoryFilter.jsx
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import {CategoryContainer} from  './styled'
+import { ThemeProvider } from '@material-ui/core'
+import theme from '../../constants/theme'
+
 
 const CategoryFilter = (props) => {
     return (
         <CategoryContainer>
+        <ThemeProvider theme={theme}>
             <Tabs
                 value={"selecionado"}
+                color={'primary'}
                 onChange={()=> null}
-                indicatorColor="primary"
-                textColor="inherit"
+                indicatorColor={'primary'}
+                textColor={'primary'}
                 variant="scrollable"
                 scrollButtons="auto"
                 aria-label="scrollable auto tabs example"
             >
-              <Tab label="Pizza"  />
+              <Tab label="Pizza" />
               <Tab label="Hamburger" />
               <Tab label="Sorvete"/>
               <Tab label="Refeição"/>
@@ -23,6 +28,7 @@ const CategoryFilter = (props) => {
               <Tab label="Salgados"/>
               <Tab label="Doces"/>
         </Tabs>
+    </ThemeProvider>
         </CategoryContainer>
     );
 }
